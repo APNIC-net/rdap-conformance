078b9a93660373ef9c5a098c866550a433acf1ff
Thomas Miller <tmiller@apnic.net>
2017-05-10 08:55:52 +1000

    Added documentation for new ContextList class.

--- src/main/java/net/apnic/rdap/conformance/ContextList.java
+++ src/main/java/net/apnic/rdap/conformance/ContextList.java
@@ -2,9 +2,24 @@ package net.apnic.rdap.conformance;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a list of Context objects.
+ *
+ * Class is a wrapper around {@link java.util.ArrayList} that provides
+ * convience functions for getting information about a list of Context objects.
+ *
+ * Note: This class does not implement any ArrayList constructors as they are
+ * not needed at present.
+ */
 public class ContextList
     extends ArrayList<Context>
 {
+    /**
+     * Indiciates if at least one Context object in this list has a failed
+     * result.
+     *
+     * @return boolean if at least one Context has a failed result.
+     */
     public boolean hasFailedResult()
     {
         for(Context context : this)
